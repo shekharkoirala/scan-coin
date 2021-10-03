d573b7fbb48909b672feae19a1018c274503f3e7
shekharkoirala <shekharkoirala4@gmail.com>
2021-10-04 00:42:56 +0545

    v 0.1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import os
+from logging import logMultiprocessing
+import os, time
 from dotenv import load_dotenv
 from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
 
@@ -9,13 +10,57 @@ class Bot:
         api_key = os.getenv('api_key')
         secret_key = os.getenv('secret_key')
         self.biclient = Client(api_key, secret_key)
+        self.total_coins = 0
 
-    def scan(self):
-        order_book_data = self.biclient.get_order_book(symbol='DCRUSDT')
-        print(order_book_data)
+    def get_usdt_coins(self):
+        """get usdt coins symbol and price
 
+        Returns:
+            [list]: eg: [{'symbol': 'SYSUSDT', 'price': '0.30610000'},{'symbol': 'FIDAUSDT', 'price': '7.23300000'}]
+        """
+        all_coin = self.biclient.get_all_tickers() 
+        selected_coin = [ coin["symbol"] for coin in all_coin if coin["symbol"][-4:] == "USDT" and coin["symbol"][-6:] != "UPUSDT" and coin["symbol"][-8:] != "DOWNUSDT"]
+        self.total_coins =  len(selected_coin)
+        return selected_coin
 
+    def get_historical_data(self, coin_name):
+        """[summary]
+
+                    1499040000000,      # Open time
+                    "0.01634790",       # Open
+                    "0.80000000",       # High
+                    "0.01575800",       # Low
+                    "0.01577100",       # Close
+                    "148976.11427815",  # Volume
+                    1499644799999,      # Close time
+                    "2434.19055334",    # Quote asset volume
+                    308,                # Number of trades
+                    "1756.87402397",    # Taker buy base asset volume
+                    "28.46694368",      # Taker buy quote asset volume
+                    "17928899.62484339" # Can be ignored
+                ]
+        iN ASCENDING DATE TIME.
+        """
+        candles = self.biclient.get_klines(symbol=coin_name, interval=Client.KLINE_INTERVAL_1HOUR, limit =4)
+        return candles
+    
+    def logic(self, history_data):
+        """ test purpose only.
+        """
+        differences = [round(float(history_data[i][4]) - float(history_data[i-1][4]), 3) for i in range(1, len(history_data))]
+        return differences, all(i<0 for i in differences)
+
+    def process(self):
+        for i , coin in enumerate(self.get_usdt_coins()):
+            data= self.get_historical_data(coin)
+            differences, status = self.logic(data)
+            if status:
+                print(coin, differences)
+            print(f"checking {i} / {self.total_coins}")
+    
 
 if __name__ == "__main__":
+    s = time.time()
     bibot = Bot()
-    bibot.scan()
+    bibot.process()
+    print(f"{time.time() -s } seconds")
---
+++ changelog.md
@@ -0,0 +1,10 @@
+v 0.1 
+
+In 67.1 seconds, we found , 
+```
+ZENUSDT [-0.36, -0.19, -0.52]
+COTIUSDT [-0.002, -0.006, -0.001]
+STRATUSDT [-0.002, -0.038, -0.009]
+```
+
+these three coins have last 4 hour of regular drop. 
